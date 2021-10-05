a99beb9576dc70d7c0a85cbe5aab183eb5a00607
Tedelyn Mula <mulatedelyn@gmail.com>
2021-10-05 12:20:22 +0800

    Initial commit

---
+++ app/Repositories/PricingRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Pricing;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class PricingRepository
+ * @package App\Repositories
+ * @version October 5, 2021, 3:03 am UTC
+*/
+
+class PricingRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'Product_name',
+        'Price'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Pricing::class;
+    }
+}
---
+++ app/Repositories/ServicesRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Services;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class ServicesRepository
+ * @package App\Repositories
+ * @version October 5, 2021, 2:18 am UTC
+*/
+
+class ServicesRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'Email',
+        'Your_email'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Services::class;
+    }
+}
